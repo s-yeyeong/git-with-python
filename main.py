f236ead3f02c28e4659c41c83bd398d7b27dd7ce
서예영 <yeyeong07031575@c4r6s1.codyssey.kr>
2026-04-07 19:12:36 +0900

    FEAT: 퀴즈 기본 데이터 추가 및 목록 출력 스위치 연결

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ class Quiz:
 class QuizGame:
     """게임 전체를 관리하는 매니저 클래스"""
     def __init__(self):
-        self.quizzes = []    # 퀴즈들을 담아둘 리스트 (장바구니)
-        self.best_score = 0  # 최고 점수
-
+        self.quizzes = [
+            Quiz("걸그룹 아이브(IVE)의 데뷔곡 제목은?", ["ELEVEN", "LOVE DIVE", "After LIKE", "I AM"], 1),
+            Quiz("다음 중 블랙핑크(BLACKPINK)의 멤버가 아닌 사람은?", ["지수", "제니", "로제", "윈터"], 4),
+            Quiz("그룹 (여자)아이들의 히트곡 'Super Lady'가 수록된 앨범 이름은?", ["I feel", "I do", "2", "I love"], 3),
+            Quiz("그룹 에스파(aespa)의 세계관에서 멤버들의 또 다른 자아를 부르는 명칭은?", ["MY", "ae", "SYNK", "P.O.S"], 2),
+            Quiz("그룹 스테이씨(STAYC)의 데뷔곡은?", ["ASAP", "SO BAD", "STEREOTYPE", "Teddy Bear"], 2)
+        ]
+        self.best_score = 0
     def display_menu(self):
         """메뉴를 화면에 보여주는 기능"""
         print("\n========================================")
@@ -39,7 +44,18 @@ class QuizGame:
 
     def show_list(self):
         """3번: 퀴즈 목록 보여주는 기능"""
-        pass
+        print(f"\n📋 등록된 퀴즈 목록 (총 {len(self.quizzes)}개)")
+        print("-" * 40)
+        
+        # 퀴즈가 하나도 없을 때
+        if len(self.quizzes) == 0:
+            print("아직 등록된 퀴즈가 없습니다.")
+        # 퀴즈가 있을 때 (for 반복문을 써서 하나씩 꺼내옵니다)
+        else:
+            for i, quiz in enumerate(self.quizzes):
+                print(f"[{i+1}] {quiz.question}")
+                
+        print("-" * 40)
 
     def show_score(self):
         """4번: 최고 점수 보여주는 기능"""
@@ -59,7 +75,7 @@ def main():
         elif choice == '2':
             print("\n준비 중인 기능입니다! (퀴즈 추가)")
         elif choice == '3':
-            print("\n준비 중인 기능입니다! (퀴즈 목록)")
+            game.show_list()
         elif choice == '4':
             print("\n준비 중인 기능입니다! (점수 확인)")
         elif choice == '5':
